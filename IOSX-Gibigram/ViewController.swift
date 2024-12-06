1b879141efa5d609e6af7e696beb0fefcc660d3d
mertziya <mert7ziya91@gmail.com>
2024-12-06 17:54:44 +0300

    Initial Commit

---
+++ IOSX-Gibigram/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  IOSX-Gibigram
+//
+//  Created by Mert Ziya on 6.12.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
