409c5ada0ef374133734867443c7237c313b0716
mertziya <mert7ziya91@gmail.com>
2024-12-18 21:05:55 +0300

    Initial Commit

---
+++ Gibigram/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Gibigram
+//
+//  Created by Mert Ziya on 18.12.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
